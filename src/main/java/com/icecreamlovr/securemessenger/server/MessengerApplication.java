e46dca2c108f7f177f9d8440f1cdd6901ff8c187
Yue He <yuehe.neu@gmail.com>
2023-07-28 21:41:29 -0700

    Implement password hashing:   - Add a @Component class to wrap password hashing logics;   - Use @Autowired and @Configuration (Spring's dependency injection) to inject singleton BCryptPasswordEncoder;   - Add a few testing endpoints to verify password hashing and verification, and as sample usage of the password hashing lib.

--- src/main/java/com/icecreamlovr/securemessenger/server/MessengerApplication.java
+++ src/main/java/com/icecreamlovr/securemessenger/server/MessengerApplication.java
@@ -3,8 +3,9 @@ package com.icecreamlovr.securemessenger.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @RestController
 public class MessengerApplication {
     public static void main(String[] args) {
