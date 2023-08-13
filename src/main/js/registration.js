963199404a53346a4c846b83a24a2b6ae0a4d25d
Yue He <yuehe.neu@gmail.com>
2023-08-12 19:04:28 -0700

    Implement UI for registration page.

--- src/main/js/registration.js
+++ src/main/js/registration.js
@@ -1,27 +1,27 @@
 import React from "react";
 import * as ReactDOMClient from 'react-dom/client';
 
-class RegistrationForm extends React.Component {
+class SignupForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: "",
       username: "",
-      password: "",
+      email: "",
+      password: ""
     };
-  }
-
-  handleEmailInput = (event) => {
-    event.preventDefault();
-    this.setState({
-      email: event.target.value
-    });
   }
 
   handleUsernameInput = (event) => {
     event.preventDefault();
     this.setState({
       username: event.target.value
+    });
+  }
+
+  handleEmailInput = (event) => {
+    event.preventDefault();
+    this.setState({
+      email: event.target.value
     });
   }
 
@@ -34,27 +34,19 @@ class RegistrationForm extends React.Component {
 
   handleSignup = (event) => {
     event.preventDefault();
-    console.log(">>>" + this.state.email + ">" + this.state.username + ">" + this.state.password + ">")
+    console.log(">>>" + this.state.email + ">" + this.state.username + ">" + this.state.password + ">");
   }
 
   render() {
     return (
-      <div className="registration-form">
-        <h2>Messenger</h2>
-        <div className="form-group">
-          <label>Email</label>
-          <input type="email" id="email" placeholder="Enter your email" onInput={this.handleEmailInput}/>
-        </div>
-        <div className="form-group">
-          <label>Username</label>
-          <input type="text" id="username" placeholder="Enter your username"  onInput={this.handleUsernameInput}/>
-        </div>
-        <div className="form-group">
-          <label>Password</label>
-          <input type="password" id="password" placeholder="Enter your password"  onInput={this.handlePasswordInput}/>
-        </div>
-        <div className="form-group">
-          <button type="button" onClick = {this.handleSignup}>Sign Up</button>
+      <div className="signup">
+        <div className="form-container">
+          <h1>Messenger</h1>
+          <h2>Create Account</h2>
+          <input type="text" id="username" placeholder="Name" onInput={this.handleUsernameInput}/>
+          <input type="email" id="email" placeholder="Email" onInput={this.handleEmailInput}/>
+          <input type="password" id="password" placeholder="Password"  onInput={this.handlePasswordInput}/>
+          <button type="button" onClick={this.handleSignup}>Sign Up</button>
         </div>
       </div>
     );
@@ -62,4 +54,4 @@ class RegistrationForm extends React.Component {
 }
 
 const root = ReactDOMClient.createRoot(document.getElementById("react-mountpoint"));
-root.render(<RegistrationForm />);
+root.render(<SignupForm />);
