020bb61e48011a04cb996193e8d66479b0e93620
Yue He <yuehe.neu@gmail.com>
2023-08-26 18:25:20 -0700

    Add request logging.

---
+++ src/main/java/com/icecreamlovr/securemessenger/server/LoggingFilter.java
@@ -0,0 +1,36 @@
+package com.icecreamlovr.securemessenger.server;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import java.io.IOException;
+
+@Component
+@Order(1)
+public class LoggingFilter implements Filter {
+    @Override
+    public void doFilter(
+            ServletRequest request,
+            ServletResponse response,
+            FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        System.out.println(String.format("[INFO] Received request: %s %s", req.getMethod(), req.getRequestURI()));
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
